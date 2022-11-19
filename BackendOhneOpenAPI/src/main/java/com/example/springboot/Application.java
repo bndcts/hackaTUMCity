9864c155f78422ef83278a8901f3a4f966f5157c
ge78sag <ge78sag@mytum.de>
2022-11-19 15:57:16 +0100

    wunderschöne development branch penis

--- BackendOhneOpenAPI/src/main/java/com/example/springboot/Application.java
+++ BackendOhneOpenAPI/src/main/java/com/example/springboot/Application.java
@@ -14,7 +14,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+/*
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
@@ -29,5 +29,7 @@ public class Application {
 
         };
     }
+ */
+
 
 }

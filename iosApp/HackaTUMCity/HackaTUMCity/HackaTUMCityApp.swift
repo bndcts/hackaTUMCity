cadaf964a05bb39cb78c3d0baf62578a5dd6dea0
Pascal Weißleder <pascal.weissleder@tum.de>
2022-11-19 09:51:24 +0100

    project setup , file structure

---
+++ iosApp/HackaTUMCity/HackaTUMCity/HackaTUMCityApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackaTUMCityApp.swift
+//  HackaTUMCity
+//
+//  Created by Pascal Weißleder on 19.11.22.
+//
+
+import SwiftUI
+
+@main
+struct HackaTUMCityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
